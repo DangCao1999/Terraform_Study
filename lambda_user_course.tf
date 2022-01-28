e6f7da009f32ac2818f898c85f3235e0268d6bdb
DangCao1999 <cao.nvd2246@sinhvien.hoasen.edu.vn>
2022-01-28 13:33:49 +0700

    GET POST Course

--- lambda_user_course.tf
+++ lambda_user_course.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "user_course_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  #source_code_hash = filebase64sha256("outputs/user_course.zip")
+  source_code_hash = filebase64sha256("outputs/user_course.zip")
 
   runtime = "nodejs14.x"
 }
